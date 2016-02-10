e4144a9e280cecfdc13c19f8940fb7ca374b92f2
Vytautas Klumbys <Vytautas.Klumbys@adform.com>
2016-02-10 10:27:00 +0200

    xml-doc's

--- src/Viki.LoadRunner.Engine/Executor/Context/Checkpoint.cs
+++ src/Viki.LoadRunner.Engine/Executor/Context/Checkpoint.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Diagnostics;
 
 namespace Viki.LoadRunner.Engine.Executor.Context
 {
+    /// <summary>
+    /// Checkpoint holds information of timestamp and error of of current iteration.
+    /// </summary>
+    [DebuggerDisplay("{Name}")]
     public class Checkpoint : ICheckpoint
     {
         #region Consts
@@ -15,14 +20,30 @@ namespace Viki.LoadRunner.Engine.Executor.Context
 
         #region Properties
 
+        /// <summary>
+        /// Name of the checkpoint (Checkpoint has const's of system checkpoints)
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Timestamp of when this checkpoint was made durring iteration
+        /// </summary>
         public TimeSpan TimePoint { get; set; }
+
+        /// <summary>
+        /// Error logged during the iteration or null.
+        /// </summary>
         public Exception Error { get; set; }
 
         #endregion
 
         #region Methods
 
+        /// <summary>
+        /// Create checkpoint
+        /// </summary>
+        /// <param name="name">name of the checkpoint</param>
+        /// <param name="timePoint">Timestamp of the checkpoint</param>
         public Checkpoint(string name, TimeSpan timePoint)
         {
             Name = name;
--- src/Viki.LoadRunner.Playground/ReplayResult.cs
+++ src/Viki.LoadRunner.Playground/ReplayResult.cs
@@ -1,17 +1,19 @@
-﻿using Viki.LoadRunner.Engine.Executor.Context;
+﻿using System.Diagnostics;
+using Viki.LoadRunner.Engine.Executor.Context;
 using Viki.LoadRunner.Engine.Executor.Result;
 
 namespace Viki.LoadRunner.Playground
 {
+    [DebuggerDisplay("T:{ThreadIterationId} G:{GlobalIterationId} L:{ThreadIterationId}")]
     public class ReplayResult<TUserData> : IterationResult
     {
-        public Checkpoint[] Checkpoints
+        public new Checkpoint[] Checkpoints
         {
             get { return (Checkpoint[]) base.Checkpoints; }
             set { base.Checkpoints = value;  }
         }
 
-        public TUserData UserData
+        public new TUserData UserData
         {
             get { return (TUserData)((IIterationMetadata<object>)this).UserData; }
             set { ((IIterationMetadata<object>)this).UserData = value; }
