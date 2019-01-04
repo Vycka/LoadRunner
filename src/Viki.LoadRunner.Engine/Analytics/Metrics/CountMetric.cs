c23b2b52f72bfe642c407a919780d1bf5383dfbb
Vytautas Klumbys <vytautas.klumbys@adform.com>
2019-01-04 15:33:23 +0200

    Added abstract count metric

---
+++ src/Viki.LoadRunner.Engine/Analytics/Metrics/CountMetric.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Viki.LoadRunner.Engine.Analytics.Interfaces;
+
+namespace Viki.LoadRunner.Engine.Analytics.Metrics
+{
+    public class CountMetric<T> : IMetric<T>
+    {
+        private readonly Func<T,bool> _canCountSelector;
+        private int _count = 0;
+
+        public CountMetric(string name = "Count")
+            : this((i) => true)
+        {
+        }
+
+        public CountMetric(Func<T,bool> canCountSelector, string name = "Count")
+        {
+            _canCountSelector = canCountSelector;
+            ColumnNames = new[] { name };
+        }
+
+        public IMetric<T> CreateNew()
+        {
+            return new CountMetric<T>(_canCountSelector, ColumnNames[0]);
+        }
+
+        public void Add(T data)
+        {
+            if (_canCountSelector(data))
+            {
+                _count++;
+            }
+        }
+
+        public string[] ColumnNames { get; }
+        public object[] Values => new object[] { _count };
+    }
+}
