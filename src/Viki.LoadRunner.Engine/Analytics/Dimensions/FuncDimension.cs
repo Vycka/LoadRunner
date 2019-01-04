be8960dfecdf7158152f0cd000d34cc85de8b47b
Vytautas Klumbys <vytautas.klumbys@adform.com>
2019-01-04 16:45:05 +0200

    Added generic FuncMetric and FuncDimension

---
+++ src/Viki.LoadRunner.Engine/Analytics/Dimensions/FuncDimension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Viki.LoadRunner.Engine.Analytics.Interfaces;
+
+namespace Viki.LoadRunner.Engine.Analytics.Dimensions
+{
+    public class FuncDimension<T> : IDimension<T>
+    {
+        private readonly Func<T, string> _dimensionValueSelector;
+
+        /// <param name="dimensionName">Name/Key of custom dimension</param>
+        /// <param name="dimensionValueSelector">Dimension value selector</param>
+        public FuncDimension(string dimensionName, Func<T, string> dimensionValueSelector)
+        {
+            if (dimensionName == null)
+                throw new ArgumentNullException(nameof(dimensionName));
+            if (dimensionValueSelector == null)
+                throw new ArgumentNullException(nameof(dimensionValueSelector));
+
+            _dimensionValueSelector = dimensionValueSelector;
+            DimensionName = dimensionName;
+        }
+
+        public string DimensionName { get; }
+
+        string IDimension<T>.GetKey(T result)
+        {
+            return _dimensionValueSelector(result);
+        }
+    }
+}
---
+++ src/Viki.LoadRunner.Engine/Analytics/Metrics/FuncMetric.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Viki.LoadRunner.Engine.Analytics.Interfaces;
+
+namespace Viki.LoadRunner.Engine.Analytics.Metrics
+{
+    public class FuncMetric<T, TValue> : IMetric<T>
+    {
+        private readonly Func<T, TValue, TValue> _metricFunc;
+        private readonly TValue _initialValue;
+
+        private TValue _value;
+
+        public FuncMetric(string keyName, TValue initialValue, Func<T, TValue, TValue> metricFunc)
+        {
+            ColumnNames = new[] {keyName};  
+            _value = initialValue;
+            _initialValue = initialValue;
+            _metricFunc = metricFunc;
+        }
+
+        public IMetric<T> CreateNew()
+        {
+            return new FuncMetric<T, TValue>(ColumnNames[0], _initialValue, _metricFunc);
+        }
+
+        public void Add(T data)
+        {
+            _value = _metricFunc(data, _value);
+        }
+
+        public string[] ColumnNames { get; }
+        public object[] Values => new object[] { _value };
+    }
+}
