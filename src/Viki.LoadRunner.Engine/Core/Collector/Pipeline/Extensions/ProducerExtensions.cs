7b4fc98136ee1c139700d30280e4ff7a69333db3
Vytautas Klumbys <vytautas.klumbys@adform.com>
2019-03-19 13:20:48 +0200

    Added some helper extensions for IProducer<T>

---
+++ src/Viki.LoadRunner.Engine/Core/Collector/Pipeline/Extensions/ProducerExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Viki.LoadRunner.Engine.Core.Collector.Pipeline.Interfaces;
+
+namespace Viki.LoadRunner.Engine.Core.Collector.Pipeline.Extensions
+{
+    public static class ProducerExtensions
+    {
+        public static Task ProduceCompleteAsync<T>(this IProducer<T> producer, IEnumerable<T> items, bool start = true)
+        {
+            return ProduceCompleteAsync(producer, items, CancellationToken.None);
+        }
+
+        public static Task ProduceCompleteAsync<T>(this IProducer<T> producer, IEnumerable<T> items, CancellationToken token, bool start = true)
+        {
+            Task task = new Task(() => ProduceAndComplete(producer, items, token), token, TaskCreationOptions.LongRunning);
+
+            if (start)
+                task.Start();
+
+            return task;
+        }
+
+        private static void ProduceAndComplete<T>(IProducer<T> producer, IEnumerable<T> items, CancellationToken token)
+        {
+            foreach (T item in items)
+            {
+                if (token.IsCancellationRequested)
+                    break;
+
+                producer.Produce(item);
+            }
+
+            producer.ProducingCompleted();
+        }
+
+        public static void Produce<T>(this IProducer<T> producer, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                producer.Produce(item);
+            }
+        }
+    }
+}
