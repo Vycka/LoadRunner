ac1f6b8541ecfff99d40a661e452b4ed6a453ab4
Vytautas Klumbys <vytautas.klumbys@adform.com>
2019-09-02 11:32:56 +0300

    Added Distinct Count metric

---
+++ src/Viki.LoadRunner.Engine/Analytics/Metrics/DistinctCountMetric.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Viki.LoadRunner.Engine.Analytics.Interfaces;
+
+namespace Viki.LoadRunner.Engine.Analytics.Metrics
+{
+    public class DistinctCountMetric<TIn, TVal> : IMetric<TIn>
+    {
+        protected readonly string _name;
+        protected readonly Func<TIn, TVal> _selector;
+
+        private readonly HashSet<TVal> _elements;
+
+        public DistinctCountMetric(Func<TIn, TVal> selector, string name = "Distinct (Count)")
+        {
+            _name = name;
+            _selector = selector;
+
+            _elements = new HashSet<TVal>();
+        }
+
+        public IMetric<TIn> CreateNew()
+        {
+            return new DistinctCountMetric<TIn, TVal>(_selector, _name);
+        }
+
+        public void Add(TIn result)
+        {
+            TVal item = _selector(result);
+
+            if (item != null && _elements.Contains(item) == false)
+            {
+                _elements.Add(item);
+            }
+        }
+
+        public string[] ColumnNames => new[] { _name };
+        public object[] Values => new object[] { _elements.Count };
+    }
+}
